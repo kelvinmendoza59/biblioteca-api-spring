de558a071b34aa11113bd0f32b316c2b5788ca98
kelvinmendoza59 <kelvinmendoza573@gmail.com>
2021-06-08 16:40:00 -0400

    Implement repository layer with Spring Data JPA

---
+++ src/main/java/com/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,13 @@
+package com.biblioteca.repository;
+
+import com.biblioteca.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    List<Libro> findByAutor(String autor);
+    List<Libro> findByTituloContaining(String titulo);
+}
